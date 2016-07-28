d69cfe3b6de23025089f14375ba3517c38d0b31a
Evin1- <edwin.hdz.12.0@gmail.com>
2016-07-28 16:25:21 -0400

    Add broadcast logic

---
+++ app/src/main/java/com/example/fragmentcommunication/BroadcastFragment.java
@@ -0,0 +1,49 @@
+package com.example.fragmentcommunication;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BroadcastFragment extends Fragment {
+
+    private Button mButton;
+
+    public BroadcastFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_broadcast, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mButton = (Button) view.findViewById(R.id.f_broadcast_btn);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.CUSTOM_EVENT_KEY);
+                LocalBroadcastManager
+                        .getInstance(getActivity())
+                        .sendBroadcast(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/fragmentcommunication/MainActivity.java
+++ app/src/main/java/com/example/fragmentcommunication/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.fragmentcommunication;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,14 +14,36 @@ public class MainActivity extends AppCompatActivity implements BlankFragment.Act
 
     private static final String TAG = "MainActivityTAG_";
 
+    public static final String CUSTOM_EVENT_KEY = "CUSTOM_ACTION_BROADCAST";
+
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "doMagic: BroadcastReceiver");
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocalBroadcastManager
+                .getInstance(this)
+                .registerReceiver(mMessageReceiver,
+                        new IntentFilter(CUSTOM_EVENT_KEY));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager
+                .getInstance(this)
+                .unregisterReceiver(mMessageReceiver);
     }
 
     @Override
     public void doMagic() {
-        Log.d(TAG, "doMagic: ");
+        Log.d(TAG, "doMagic: Interface");
     }
 }
